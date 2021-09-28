c2bae7535039a48f7570d5a49633d4b8281ac4da
Ismaeel Yaghoubi <e30.sa23@gmail.com>
2021-09-28 17:34:06 +0200

    Login and Register pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './Pages/Home/Home.jsx';
 import Single from "./Pages/Single/Single.jsx";
 import Write from "./Pages/Write/Write.jsx";
 import Settings from "./Pages/Settings/Settings.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import Register from "./Pages/Register/Register.jsx";
 
 function App() {
   return (
     <>
     <Header />
-    <Settings />
+    <Register />
     </>
   );
 }
