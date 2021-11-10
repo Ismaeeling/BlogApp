6ef3425967e5144b1c379d961d6d06719ec9b754
Ismaeel Yaghoubi <e30.sa23@gmail.com>
2021-11-10 16:18:32 +0100

    Posts loads dynamically

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -1,19 +1,26 @@
-import './post.css'
+import './post.css';
+import {Link} from "react-router-dom";
 
-export default function Post() {
+export default function Post({post}) {
     return (
         <div className="post">
-            <div className="postImg">
-                <img src="https://www.topgear.com/sites/default/files/images/news-article/2019/09/8b9981f2be8ad1d2c90af3dfb83e4f74/02_ss300p_3i4_front.jpg" alt="" />
-            </div>
+            {post.photo && (
+                <div className="postImg">
+                    <img src="{post.photo}" alt="" />
+                </div>
+            )}
+            
             <div className="postInfo">
                 <div className="postCats">
-                    <span className="postCat">Life</span>
-                    <span className="postCat">Music</span>
+                    {post.categories.map(c=>(
+                        <span className="postCat">{c.name}</span>
+                    ))}
                 </div>
-                <span className="postTitle">Lorem Ipsum</span>
-                <span className="postPublish">1 hour ago</span>
-                <span className="postDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, magnam beatae nobis laudantium dicta molestias fuga perspiciatis ducimus quasi aut reprehenderit corrupti ad nemo? Sit molestias illo numquam atque porro.</span>
+                <Link to={`/post/${post._id}`} className="link">
+                    <span className="postTitle">{post.title}</span>
+                </Link>
+                <span className="postPublish">{new Date(post.createdAt).toDateString()}</span>
+                <span className="postDesc">{post.desc}</span>
             </div>
         </div>
     )
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import './home.css';
 import Hero from '../../Components/Hero/Hero';
 import Posts from '../../Components/Posts/Posts';
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import axios from "axios";
 
 export default function Home() {
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+        const fetchPosts = async ()=>{
+            const res = await axios.get("/posts")
+            setPosts(res.data);
+        }
+        fetchPosts();
+    },[])
     return (
         <>
         <Hero />
         <div className="home">
-            <Posts />
+            <Posts posts={posts} />
             <Sidebar />
         </div>
         </>
